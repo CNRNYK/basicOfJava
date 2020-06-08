cacfbd37be174c87945a2cb49ec2fba28c51127e
cnrnyk <Git01Gma02!>
2020-06-08 16:18:10 +0300

    test

--- src/days_tasks/day_0010/forLoops.java
+++ src/days_tasks/day_0010/forLoops.java
@@ -62,6 +62,11 @@ public class forLoops {
                 System.out.print(z + " ");
             }
         }
+
+        for(int i=5; i<=140 && i%2==1;i+=2) {
+            System.out.print(i+" ");
+        }
+
         System.out.println("\n");
 
         /*
